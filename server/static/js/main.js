3f2604080698c22e4343ced1ac30ae2ae3b42a3c
Matt Schmoyer <mschmoyer@50onred.com>
2014-09-20 20:43:34 -0400

    Adds main page link

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -1,35 +1,28 @@
-/*
-*  Matthew Schmoyer
-*  Aug 17 2013
-*
-*/
 
 $(document).ready(function() {
-	console.log('Locked and Loaded');
-
-	$('#tab_blog').on('click', function() {
-		if ($(this).hasClass('active'))
-			return false;
-		$("a[id^='tab_']").removeClass('active');
-		$(this).addClass('active');
-		$("div[id^='tab_body_']").hide();
-		$('#tab_body_blog').fadeIn('slow');
-	});
-	$('#tab_portfolio').on('click', function() {
-		if ($(this).hasClass('active'))
-			return false;
-		$("a[id^='tab_']").removeClass('active');
-		$(this).addClass('active');
-		$("div[id^='tab_body_']").hide();
-		$('#tab_body_portfolio').fadeIn('slow');
-	});
-	$('#tab_contact').on('click', function() {
-		if ($(this).hasClass('active'))
-			return false;
-		$("a[id^='tab_']").removeClass('active');
-		$(this).addClass('active');
-		$("div[id^='tab_body_']").hide();
-		$('#tab_body_contact').fadeIn('slow');
-	});
+    $('#tab_blog').on('click', function() {
+        if ($(this).hasClass('active'))
+            return false;
+        $("a[id^='tab_']").removeClass('active');
+        $(this).addClass('active');
+        $("div[id^='tab_body_']").hide();
+        $('#tab_body_blog').fadeIn('slow');
+    });
+    $('#tab_portfolio').on('click', function() {
+        if ($(this).hasClass('active'))
+            return false;
+        $("a[id^='tab_']").removeClass('active');
+        $(this).addClass('active');
+        $("div[id^='tab_body_']").hide();
+        $('#tab_body_portfolio').fadeIn('slow');
+    });
+    $('#tab_contact').on('click', function() {
+        if ($(this).hasClass('active'))
+            return false;
+        $("a[id^='tab_']").removeClass('active');
+        $(this).addClass('active');
+        $("div[id^='tab_body_']").hide();
+        $('#tab_body_contact').fadeIn('slow');
+    });
 
 });
