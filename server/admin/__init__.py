817e6dd5125c0b0da829e6ace31d7c177ca1a916
Matt Schmoyer <mattschmo@gmail.com>
2015-08-29 14:51:39 -0400

    adds vegas slideshow

--- server/admin/__init__.py
+++ server/admin/__init__.py
@@ -2,8 +2,12 @@ from flask import Blueprint, render_template
 
 
 admin = Blueprint('admin', __name__, url_prefix='/admin', template_folder='templates',
-	static_folder='static')
+    static_folder='static')
 
 @admin.route('/')
 def index():
-	return render_template('admin/index.html')
+    return render_template('admin/index.html')
+
+@admin.route('/screen')
+def schmo_screen():
+    return render_template('admin/schmo_screen.html')
